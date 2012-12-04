24b832cab193fea4069330cec067fb34bc663c65
Philipp Hanslovsky <Philipp.Hanslovsky@gmail.com>
2012-12-04 23:26:55 +0100

    updated numpy2tex.py

--- numpy2tex.py
+++ numpy2tex.py
@@ -1,3 +1,5 @@
+# written by Philipp Hanslovsky
+
 import numpy as np
 
 def numpy2tex(array,
@@ -8,7 +10,13 @@ def numpy2tex(array,
               columnTitles = [],
               rowTitles = [],
               upperLeft = ''):
+    """convert numpy 2D array to tex table
+    If filename is specified, the tex table will be written to that file; otherwise the string is returned."""
+
+    # convert any 2D iterable to np.array
+    array = np.array(array)
     m, n = array.shape
+    # add titles for Columns, Rows and upper left cell, if given/neccessary
     cTitles = False
     if columnTitles:
         array = np.append(np.array(columnTitles)[np.newaxis, :], array, axis = 0)
@@ -17,11 +25,12 @@ def numpy2tex(array,
         if cTitles:
             rowTitles = np.append([upperLeft], rowTitles)[:, np.newaxis]
         array = np.append(rowTitles, array, axis = 1)
+
+    # convert to tex string
     texString = \
         "\\begin{table}[h!]\n" + \
         "\\centering\n" + \
         "\\begin{tabular}{" + columns + "}\n\\hline\n"
-    
     for line in array:
         for el in line[:-1]:
             texString += el + " & "
@@ -31,11 +40,11 @@ def numpy2tex(array,
         "\\caption{" + caption + "}\n" + \
         "\\label{tab:" + label + "}\n" + \
         "\\end{table}"
+    # write to file if specified and return string
     if filename:
         with open(filename, 'w') as f:
             f.write(texString)
-    else:
-        return texString
+    return texString
     
     
 if __name__ == "__main__":
