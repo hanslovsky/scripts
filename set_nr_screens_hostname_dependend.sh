bdcb16a5be073e090cff93fecd10768a49077b08
Philipp Hanslovsky <Philipp.Hanslovsky@iwr.uni-heidelberg.de>
2013-05-29 10:57:00 +0200

    now using two dvi outputs

--- set_nr_screens_hostname_dependend.sh
+++ set_nr_screens_hostname_dependend.sh
@@ -8,6 +8,6 @@ if [[ "$name" -eq "Conan" ]]; then
     xrandr --output VGA1 --auto --right-of DVI1
 fi
 
-if [[ "$name" -eq "vaio81313" ]]; then
-    xrandr --output VGA1 --auto --right-of
+if [[ "$name" -eq "Kaminsky" ]]; then
+    xrandr --output DVI2 --auto --right-of DVI2
 fi
