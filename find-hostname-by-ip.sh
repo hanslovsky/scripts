7f7cbebd15a7c8b381f0bb2b459e224018a25803
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-01-20 15:52:29 -0500

    find hostname based on ip

---
+++ find-hostname-by-ip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CWD="$PWD"
+# LOG_DIR something like ~/.spark/logs/<timestamp>
+LOG_DIR="$1"
+IP_ADDR="$2"
+
+cd $LOG_DIR
+
+for fn in h*.out; do
+    HN=`echo $fn | grep -Eo '^[^\.]+'`
+    ssh -x $HN '/sbin/ip addr | grep "10.38.104.35" && hostname';
+done
+
+cd $CWD
